91e9ef262435705667df8bdcdb904bcd7eef3c03
Rileyc2022 <rileyc2022@headroyce.org>
2021-12-08 10:50:06 -0800

    Animated ping response

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,4 +1,4 @@
 import { Message } from "discord.js";
 export const ping_command = (msg: Message) => {
-  msg.channel.send("Pong!")
+  msg.channel.send("Pong! <a:animated_sparkles:918211295986217060>")
 }
